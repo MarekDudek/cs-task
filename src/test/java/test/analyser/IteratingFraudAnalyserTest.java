71edfe0d895846a79dd5e2afc363b20549f9564b
Marek Dudek <marek.dudek@gmail.com>
2014-12-27 19:22:53 +0100

    Tested individual analysis

--- src/test/java/test/analyser/IteratingFraudAnalyserTest.java
+++ src/test/java/test/analyser/IteratingFraudAnalyserTest.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.assertThat;
 import static test.analyser.TestData.BLACKLISTED_USER_ON_DUE_DAY;
 import static test.analyser.TestData.REGULAR_USER_ON_DUE_DAY;
 import static test.analyser.TestData.SKIP_ANALYSIS;
+import static test.analyser.TestData.SUSPECT_INDIVIDUALLY;
 import static test.analyser.TestData.VARIOUS_TRANSACTIONS;
 
 import java.util.Iterator;
@@ -54,6 +55,39 @@ public class IteratingFraudAnalyserTest {
 	// then
 	assertThat(suspicious.next(), is(BLACKLISTED_USER_ON_DUE_DAY));
 	assertThat(suspicious.next(), is(REGULAR_USER_ON_DUE_DAY));
+
+	assertThat(suspicious.next(), any(Transaction.class));
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void iteration_with_checking_if_next_exists__when_not_all_are_suspected()
+    {
+	// given
+
+	analyser = new IteratingFraudAnalyser(SKIP_ANALYSIS, SUSPECT_INDIVIDUALLY);
+
+	// when
+	final Iterator<Transaction> suspicious = analyser.analyse(VARIOUS_TRANSACTIONS.iterator(), null);
+
+	// then
+	assertThat(suspicious.hasNext(), is(true));
+	assertThat(suspicious.next(), is(BLACKLISTED_USER_ON_DUE_DAY));
+	assertThat(suspicious.hasNext(), is(false));
+
+	assertThat(suspicious.next(), any(Transaction.class));
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void iteration_without_checking_if_next_exists__when_not_all_are_suspected()
+    {
+	// given
+	analyser = new IteratingFraudAnalyser(SKIP_ANALYSIS, SUSPECT_INDIVIDUALLY);
+
+	// when
+	final Iterator<Transaction> suspicious = analyser.analyse(VARIOUS_TRANSACTIONS.iterator(), null);
+
+	// then
+	assertThat(suspicious.next(), is(BLACKLISTED_USER_ON_DUE_DAY));
 
 	assertThat(suspicious.next(), any(Transaction.class));
     }
