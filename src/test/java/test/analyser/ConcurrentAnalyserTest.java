0618229aa0e39685d57dc28870c804ac553dc359
Marek Dudek <marek.dudek@gmail.com>
2014-12-28 18:43:19 +0100

    Grouping by with lambdas

--- src/test/java/test/analyser/ConcurrentAnalyserTest.java
+++ src/test/java/test/analyser/ConcurrentAnalyserTest.java
@@ -2,6 +2,7 @@ package test.analyser;
 
 import static com.google.common.collect.Lists.newArrayList;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
 import static org.junit.Assert.assertThat;
 import static solution.PredicateFactory.belongsTo;
 import static solution.PredicateFactory.sameDate;
@@ -9,11 +10,14 @@ import static test.analyser.TestGeneratorSettings.BLACKLISTED_COUNT;
 import static test.analyser.TestGeneratorSettings.CONFIG;
 import static test.analyser.TestGeneratorSettings.DUE_DAY;
 import static test.analyser.TestGeneratorSettings.NUMBER_OF_TRANSACTIONS;
+import static test.analyser.TestGeneratorSettings.USER_COUNT;
 import static test.analyser.TestGeneratorSettings.WHITELISTED_COUNT;
 
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 import org.junit.Test;
 
@@ -45,5 +49,26 @@ public class ConcurrentAnalyserTest {
 
 	// then
 	assertThat(count, equalTo(33339L));
+    }
+
+    @Test
+    public void transaction_count_from_account()
+    {
+	final TransactionGenerator generator = new TransactionGenerator(CONFIG);
+
+	final List<Long> whitelisted = generator.chooseWhitelisted(WHITELISTED_COUNT);
+	final Predicate<Transaction> skipAnalysis = belongsTo(whitelisted).or(sameDate(DUE_DAY).negate());
+
+	final Iterator<Transaction> result = generator.generateIterator(NUMBER_OF_TRANSACTIONS);
+	final List<Transaction> transactions = newArrayList(result);
+
+	// when
+	final Map<Long, List<Transaction>> transactionsPerUser = transactions
+		.stream()
+		.filter(skipAnalysis.negate())
+		.collect(Collectors.groupingBy(Transaction::getUserId));
+
+	// then
+	assertThat(transactionsPerUser.keySet(), hasSize(USER_COUNT - WHITELISTED_COUNT - 1));
     }
 }
